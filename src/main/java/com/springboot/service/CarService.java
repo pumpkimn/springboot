35d5a5c85a12e7715060560fb1fd8de8c3ea2e55
pumpkimn <794663592@qq.com>
2018-04-04 08:48:51 +0800

    work

--- src/main/java/com/springboot/service/CarService.java
+++ src/main/java/com/springboot/service/CarService.java
@@ -14,10 +14,10 @@ import java.util.List;
 @Service
 public class CarService {
     public List<Car> list() {
-        Car car = new Car("法拉利", 558d, new Date());
-        Car car1 = new Car("兰博基尼", 558d, new Date());
-        Car car2 = new Car("奥托", 558d, new Date());
-        Car car3 = new Car("奥迪", 558d, new Date());
+        Car car = new Car(1,"法拉利", 558d, new Date());
+        Car car1 = new Car(2,"兰博基尼", 558d, new Date());
+        Car car2 = new Car(3,"奥托", 558d, new Date());
+        Car car3 = new Car(4,"奥迪", 558d, new Date());
 
 
         List<Car> list = new ArrayList<>();
